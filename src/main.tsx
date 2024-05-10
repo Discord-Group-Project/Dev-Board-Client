33cf85bddebf4bcb73887a66ad41707df744b3ca
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-05-10 21:50:01 +0530

    add tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./styles/global.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Hello World </h1>
+    <h1 className="text-2xl">Hello World </h1>
   </React.StrictMode>
 );
