0c313b494401c6315646870e59179e2448da54ba
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-06-22 16:18:53 +0530

    verify email complete

--- src/app/auth/email/verify/page.tsx
+++ src/app/auth/email/verify/page.tsx
@@ -1,26 +1,61 @@
 "use client";
 import Link from "next/link";
 import { MdOutlinePhoneAndroid } from "react-icons/md";
+import { useVerifyEmailForm, VerifyEmailFormSchemaType } from "@/hooks/form";
+import { Api } from "@/lib";
+import toast from "react-hot-toast";
+import { useMutation } from "@tanstack/react-query";
 
 function Page() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useVerifyEmailForm();
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["verifyEmail"],
+    mutationFn: (data: VerifyEmailFormSchemaType) =>
+      Api.post("/users/email-verify", data).then((res) => res.data),
+    onSuccess: (data: any) => {
+      toast.success(data.message);
+      window.location.replace("/auth/signin");
+    },
+    onError: (error: any) => {
+      toast.error(error?.response?.data?.message || error?.message);
+    },
+  });
+
+  const handleVerifyEmail = (data: VerifyEmailFormSchemaType) => {
+    mutate(data);
+  };
   return (
     <>
       <section className="flex justify-center items-center h-screen mx-2">
-        <form className="flex gap-8 flex-col bg-gray-800 rounded-lg p-3 w-full md:w-1/2 lg:w-1/3 m-1">
+        <form
+          className="flex gap-8 flex-col bg-gray-800 rounded-lg p-3 w-full md:w-1/2 lg:w-1/3 m-1"
+          onSubmit={handleSubmit(handleVerifyEmail)}
+        >
           <h1 className="text-center font-semibold text-xl">Verify Email</h1>
 
           <div>
             <label className="input input-bordered flex items-center gap-2">
               <MdOutlinePhoneAndroid size={20} />
-              <input type="text" className="grow" placeholder="OTP" />
+              <input
+                type="text"
+                className="grow"
+                placeholder="OTP"
+                {...register("otp")}
+              />
             </label>
-
-            <p className="text-red-500 font-medium"></p>
+            {errors?.otp && (
+              <p className="text-red-500 font-medium">{errors?.otp?.message}</p>
+            )}
           </div>
 
           <div>
             <button type="submit" className="btn btn-sm btn-primary w-full">
-              Verify Email
+              [{isPending ? "Loading..." : "Verify"}]
             </button>
           </div>
 
---
+++ src/hooks/form/verifyEmail.ts
@@ -0,0 +1,22 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const schema = z.object({
+  otp: z
+    .string({ required_error: "OTP required" })
+    .min(6, { message: "OTP most be 6 characters" }),
+});
+
+export type VerifyEmailFormSchemaType = z.infer<typeof schema>;
+
+function useVerifyEmailForm() {
+  return useForm<VerifyEmailFormSchemaType>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      otp: "",
+    },
+  });
+}
+
+export { useVerifyEmailForm };
